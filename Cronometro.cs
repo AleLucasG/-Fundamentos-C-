386839e29737c1f982197c38c62225e8a29b0d1c
Alessandra Lucas <95228437+AleLucasG@users.noreply.github.com>
2022-08-04 16:31:44 -0300

    Ajustes

--- Cronometro.cs
+++ Cronometro.cs
@@ -7,22 +7,40 @@ namespace Cronometro
     {
         static void Main(string[] args)
         {
-            Start(10);
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(" ====== Cronômetro ====== ");
+            Console.WriteLine("");
+            Console.WriteLine("S = Segundos");
+            Console.WriteLine("M = Minutos");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar: ");
+            Console.ResetColor();
+
+            Console.ReadKey();
         }
 
         static void Start(int time) // inclui um paramentro
         {
-            int currentTime = 0; // Incremento
+            int currentTime = 0; 
 
             while (currentTime != time) //sempre executa aldo dada uma condição e ela tem que ser True.
             {
-
+                Console.Clear();
                 currentTime++;
                 Console.WriteLine(currentTime);
-                Thread.Sleep(300);
+                Thread.Sleep(2000);
             }
-            Console.ReadKey();
 
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Cronômetro finalizado");
+            Thread.Sleep(2500);
         }
     }
 }
