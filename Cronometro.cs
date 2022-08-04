428998be1331e411d0974a19ea4e753e8b2115d0
Alessandra Lucas <95228437+AleLucasG@users.noreply.github.com>
2022-08-04 14:50:08 -0300

    Add files via upload

---
+++ Cronometro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace Cronometro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start(int time)
+        {
+            int time = 10;
+            int currentTime = 0; // Incremento
+
+            while (currentTime != time) //sempre executa aldo dada uma condição e ela tem que ser True.
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
