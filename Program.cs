e7347b95d9f687712c32d4299270dad3558f0bde
Alessandra Lucas <95228437+AleLucasG@users.noreply.github.com>
2022-08-04 12:29:44 -0300

    Ajuste calculadora

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Calculadora
+namespace TesteAulaBalta
 {
     internal class Program
     {
@@ -8,20 +8,28 @@ namespace Calculadora
         {
             Menu();
         }
-
         static void Menu()
         {
             Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Qual calculo deseja efetuar?");
+            Console.ResetColor();
+            Console.WriteLine("");
+
+            Console.ForegroundColor= ConsoleColor.Green;
             Console.WriteLine("1 - Soma");
             Console.WriteLine("2 - Subtração");
-            Console.WriteLine("3 - Divisão");
-            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
             Console.WriteLine("5 - Sair");
-            Console.WriteLine();
+            Console.ResetColor();
+            Console.WriteLine("");
 
-            Console.Write("Selecione uma opção: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Digite a opção escolhida: ");
             short resp = short.Parse(Console.ReadLine());
+            Console.ResetColor();
+            Console.WriteLine();
 
             switch (resp)
             {
@@ -29,90 +37,98 @@ namespace Calculadora
                     break;
                 case 2: Subtracao();
                     break;
-                case 3: Divisao();
+                case 3: Multiplicacao();
                     break;
-                case 4: Multiplicacao();
+                case 4: Divisao();
                     break;
-                case 5: Environment.Exit(0); //System.Environment.Exit(0)
+                case 5: System.Environment.Exit(0);
                     break;
-                default: Menu(); // caso a opção seja digitado errado, permance no Menu.
+                default: Menu();
                     break;
             }
 
             Console.ReadKey();
         }
-        static void Soma()
+        
+       static void Soma()
         {
             Console.Clear();
 
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Calculo de Soma");
-            Console.Write("Primeiro Valor: ");
-            float v1 = float.Parse(Console.ReadLine());
-
-            Console.Write("Segundo valor: ");
-            float v2 = float.Parse(Console.ReadLine());
-
-            float resultado = v1 + v2;
-            // Console.WriteLine("Resultado da soma é: " + resultado);
-            Console.WriteLine($"Resultado da soma é: {resultado}."); //interpolação de string
-
-            Console.ReadKey();
-            Menu();
-        }
-
-        static void Subtracao()
-        {
-            Console.Clear();
-
-            Console.WriteLine("Calculo de Subtração");
-            Console.Write("Primeiro valor: ");
-            float v1 = float.Parse(Console.ReadLine());
-
-            Console.Write("Segundo valor: ");
-            float v2 = float.Parse(Console.ReadLine());
-
-            float resultado = v1 - v2;
-            Console.WriteLine($"Resultado da subtração é: {resultado}.");
-
-            Console.ReadKey();
-            Menu();
-        }
-
-        static void Divisao()
-        {
-            Console.Clear();
-
-            Console.WriteLine("Calculo de Divisão");
-            Console.Write("Primeiro valor: ");
-            float v1 = float.Parse(Console.ReadLine());
-
-            Console.Write("Segundo valor: ");
-            float v2 = float.Parse(Console.ReadLine());
-
-            float resultado = v1 / v2;
-            Console.WriteLine($"Resultdo da divisão é: {resultado}.");
-
-            Console.ReadKey();
-            Menu();
-
-        }
-        static void Multiplicacao()
-        {
-            Console.Clear();
-
-            Console.WriteLine("Calculo de Multiplicação");
             Console.Write("Digite o primeiro valor: ");
-            float v1 = float.Parse(Console.ReadLine());
+            float valor1 = float.Parse(Console.ReadLine());
 
             Console.Write("Digite o segundo valor: ");
-            float v2 = float.Parse(Console.ReadLine());
+            float valor2 = float.Parse(Console.ReadLine());
 
-            float resultado = v1 * v2;
-            Console.WriteLine($"Resultado da multiplicação é: {resultado}");
+            float result = valor1 + valor2;
+            Console.WriteLine($"Resultado da Soma é: {result}");
 
             Console.ReadKey();
             Menu();
         }
+
+       static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Calculo de Subtração");
+            Console.Write("Escreva o primeiro valor: ");
+            float valor1 = float.Parse((string)Console.ReadLine());
+
+            Console.Write("Escreva o segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            float result = valor1 - valor2;
+            Console.WriteLine($"O resultado da Subtração é: {result}");
+
+            Console.ReadKey();
+            Menu();
+        }
+
+       static void Multiplicacao()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Calculo de Multiplicação");
+
+            Console.Write("Digite o primeiro valor: ");
+            float valor1 = float.Parse((string)Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            float valor2 = float.Parse(((string)Console.ReadLine()).Trim());
+
+            float result = valor1 * valor2;
+            Console.WriteLine($"Resulta da Multiplicação é: {result}");
+
+            Console.ReadKey();
+            Menu();
+        }
+
+        
+       static void Divisao()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Calculo de Divisão");
+
+            Console.Write("Digite o primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            float result = valor1 / valor2;
+            Console.WriteLine($"O resultado da Divisão é: {result}");
+            Console.ResetColor();
+
+            Console.ReadKey();
+            Menu();
+        } 
     }
 
 }
+
+
