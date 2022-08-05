f7307ce4237a3d05c49339aff033f3578bdb62e2
Alessandra Lucas <95228437+AleLucasG@users.noreply.github.com>
2022-08-05 18:27:49 -0300

    Ajustes

--- Cronometro.cs
+++ Cronometro.cs
@@ -13,34 +13,48 @@ namespace Cronometro
         static void Menu()
         {
             Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Green;
+
             Console.WriteLine(" ====== Cronômetro ====== ");
             Console.WriteLine("");
-            Console.WriteLine("S = Segundos");
-            Console.WriteLine("M = Minutos");
+            Console.WriteLine("S = Segundo => 10seg = 10 segundos");
+            Console.WriteLine("M = Minuto => 1min = 60 segundos");
             Console.WriteLine("0 = Sair");
-            Console.WriteLine("Quanto tempo deseja contar: ");
-            Console.ResetColor();
+            Console.Write("Quanto tempo deseja contar: ");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length -1, 1));
+            int time = int.Parse(data.Substring(0, data.Length -1));
+            int multiplicador = 1;
+
+            if (type == 'm')
+                multiplicador = 60;
+
+            if(time == 0)
+                Environment.Exit(0);
+
+            Start(time * multiplicador);
 
             Console.ReadKey();
         }
 
-        static void Start(int time) // inclui um paramentro
+        static void Start(int time)
         {
-            int currentTime = 0; 
+            // int time = 10; - foi declado na função Start //
+            int currentTime = 0;
 
-            while (currentTime != time) //sempre executa aldo dada uma condição e ela tem que ser True.
+            while (currentTime != time)
             {
                 Console.Clear();
                 currentTime++;
                 Console.WriteLine(currentTime);
-                Thread.Sleep(2000);
+                Thread.Sleep(1000);
             }
 
             Console.ReadKey();
             Console.Clear();
             Console.WriteLine("Cronômetro finalizado");
             Thread.Sleep(2500);
+            Menu();
         }
     }
 }
