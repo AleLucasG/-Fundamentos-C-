70f079d43eb05aed6ee570704dd741daf8309994
Alessandra Lucas <95228437+AleLucasG@users.noreply.github.com>
2022-08-11 21:04:04 -0300

    Ajustes

--- EditorDeTexto.cs
+++ EditorDeTexto.cs
@@ -37,7 +37,18 @@ namespace EditorDeTexto
 
         static void Abrir()
         {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo? ");
+            string path = Console.ReadLine();
 
+            using (var file = new StreamReader(path))
+            {
+                string texto = file.ReadToEnd();
+                Console.WriteLine(texto);
+            }
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
         }
 
         static void Editar()
